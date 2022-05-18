f9a699afe6a98452158e74274688ef8ab1091090
Hagler Wafula <haglerwafula@gmail.com>
2022-05-18 23:58:53 +0300

    update delete endpoint httpstatus response

--- src/main/java/com/caehag/supportportal/resource/UserResource.java
+++ src/main/java/com/caehag/supportportal/resource/UserResource.java
@@ -106,7 +106,7 @@ public class UserResource extends ExceptionHandling {
     @PreAuthorize("hasAnyAuthority('user:delete')")
     public ResponseEntity<HttpResponse> deleteUser(@PathVariable("id") long id) {
         userService.deleteUser(id);
-        return response(NO_CONTENT, USER_DELETED_SUCCESSFULLY);
+        return response(OK, USER_DELETED_SUCCESSFULLY);
     }
 
     @PostMapping("/update-profile-image")
